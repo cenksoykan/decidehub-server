0deb9d0b6589b3b328a4763d4d1fae3c62dffb5c
Ali Tas <ali.tas@edelkrone.com>
2020-07-21 12:05:36 -0400

    Added members controller

---
+++ src/Decidehub.Web/Controllers/Api/ApiMemberController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Decidehub.Web.Interfaces;
+using Decidehub.Web.ViewModels.Api;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Decidehub.Web.Controllers.Api
+{
+    [Route("api/v1/members")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class MemberController : Controller
+    {
+        private readonly IUserApiViewModelService _userViewModelService;
+
+        public MemberController(IUserApiViewModelService userViewModelService)
+        {
+            _userViewModelService = userViewModelService;
+        }
+
+        /// <summary>
+        ///     Gets member list
+        /// </summary>
+        /// <returns>Member List</returns>
+        /// <response code="200">Member List</response>
+        [ProducesResponseType(typeof(IEnumerable<UserViewModel>), 200)]
+        [HttpGet]
+        public async Task<IEnumerable<MemberViewModel>> Get()
+        {
+            var users = await _userViewModelService.ListUsersWithImages();
+
+            return users.Where(r => r.IsActive).Select(r => new MemberViewModel
+            {
+                Id = r.Id,
+                Email = r.Email,
+                FirstName = r.FirstName,
+                LastName = r.LastName,
+                UserImage = r.UserImage
+            });
+        }
+    }
+}
---
+++ src/Decidehub.Web/ViewModels/Api/MemberViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Decidehub.Web.ViewModels.Api
+{
+    public class MemberViewModel : BaseViewModel<string>
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserImage { get; set; }
+    }
+}
